cc7e0c70aea53f447c64cf67cad0fc0b683d1044
SuJeong Moon <msj1506@naver.com>
2022-04-03 16:11:16 +0900

    [Bronze II] Title: 나머지, Time: 208 ms, Memory: 17612 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,15 +1,23 @@
-import java.util.HashSet;
 import java.util.Scanner;
 
 class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		HashSet<Integer> set = new HashSet<Integer>();
- 
-		for (int i = 0; i < 10; i++) {
-			set.add(sc.nextInt() % 42);
-		}
+	
+		
+        boolean[] arr = new boolean[42];
+        Scanner sc = new Scanner(System.in);
         
-		System.out.print(set.size());
+        for(int i = 0 ; i < 10 ; i++) {
+            arr[sc.nextInt() % 42] = true;
+        }
+        
+        int count = 0;
+        for(boolean value : arr) {
+            if(value){    
+                count++;
+            }
+        }
+        System.out.println(count);
+    
 	}
 }
