c43162c9b8a3b7d3fd9df67724967fef12a88eb5
SuJeong Moon <msj1506@naver.com>
2022-04-03 16:26:40 +0900

    [Bronze I] Title: 평균, Time: 260 ms, Memory: 18740 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,20 +1,21 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++) {
-        	arr[i]=sc.nextInt();
-        }
-    
-        Arrays.sort(arr);
-        double sum = 0;
-        for(int i=0;i<n;i++) {
-        	sum+=((arr[i] / (double)arr[arr.length-1]) * 100);
-        }
-        System.out.print(sum / arr.length);
+    	int max = -1;
+		double sum = 0.0;
+		
+		for (int i = 0; i < n; i++) {
+			int value = sc.nextInt();
+			
+			if(value > max) {
+				max = value;
+			}
+			
+			sum += value;
+		}
+		System.out.println(((sum / max) * 100.0) / n);
 	}
 }
