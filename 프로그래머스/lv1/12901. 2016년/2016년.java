dd747ca555cb6d68b39207504ae3b2f41b0a55f1
SuJeong Moon <msj1506@naver.com>
2022-06-07 09:58:05 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,14 @@
+
+class Solution {
+    public String solution(int a, int b) {
+    	int[] day= {31,29,31,30,31,30,31,31,30,31,30,31};
+    	String[] week= {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+    	int sum=0;
+    	for(int i=0;i<a-1;i++) {
+    		sum+=day[i];
+    	}
+    	sum+=b-1;
+    	return week[sum%7];
+    	
+    }
+}
