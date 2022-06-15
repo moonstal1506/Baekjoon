f2321420b51244b1b79b7c1e26390d9a23de7c41
SuJeong Moon <msj1506@naver.com>
2022-06-15 10:41:12 +0900

    [level 2] Title: 올바른 괄호, Time: 17.67 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+
+    	Stack<Character> stack = new Stack<>();
+    	for(char x:s.toCharArray()) {
+    		if(x=='(') {
+    			stack.push(x);
+    		}else {
+    			if(stack.isEmpty()) {
+    				return false;
+    			}
+    			stack.pop();
+    		}
+    	}
+        if(!stack.isEmpty()) return false;
+
+        return true;
+    }
+}
