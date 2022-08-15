19e399d25ffd681b32027cb6d3bcaf3a08e6acb0
SuJeong Moon <msj1506@naver.com>
2022-08-16 08:49:04 +0900

    [level 2] Title: 프린터, Time: 1.89 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/README.md
+++ 프로그래머스/lv2/42587. 프린터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프린터 - 42587 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/42587) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 75.4 MB, 시간: 1.56 ms
+메모리: 72.9 MB, 시간: 1.89 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42587. 프린터/프린터.java
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -1,38 +1,34 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        
-        //어레이리스트에 대기목록 담기
-        List<Job> list = new ArrayList<>();
-        for(int i=0;i<priorities.length;i++) {
-        	list.add(new Job(priorities[i],i));
+        //대기목록 담기
+        Queue<Printer> q = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++) {
+            q.add(new Printer(priorities[i], i));
         }
-        
         //비어있지 않을 때까지 돌고 앞에꺼 꺼내서 가장 큰지 확인 작으면 다시 뒤로 크면 ++ 찾았으면 멈춰
-        while(!list.isEmpty()) {
-        	Job job = list.remove(0);
-        	if(list.stream().anyMatch(i->i.priority>job.priority)) {
-        		list.add(job);
-        	}else {
-        		answer++;
-        		if(job.location==location) {
-        			break;
-        		}
-        	}
+        while (!q.isEmpty()) {
+            Printer printer = q.poll();
+            if (q.stream().anyMatch(i -> i.priority > printer.priority)) {
+                q.add(printer);
+            }else {
+                answer++;
+                if(printer.location==location) break;
+            }
         }
         return answer;
     }
-    
-    static class Job{
-    	int priority;
-    	int location;
-    	
-		public Job(int priority, int location) {
-			this.priority = priority;
-			this.location = location;
-		}
+
+    static class Printer{
+        int priority;
+        int location;
+
+        public Printer(int priority, int location) {
+            this.priority = priority;
+            this.location = location;
+        }
     }
 }
