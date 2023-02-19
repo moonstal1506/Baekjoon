8635450c25fb104432651e7a3b06542263facbde
SuJeong Moon <moonstal1506@gmail.com>
2023-02-19 13:12:20 +0900

    [Silver II] Title: 병사 배치하기, Time: 160 ms, Memory: 14632 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] power;
+
+    public static void main(String[] args) throws IOException {
+        //입력
+        input();
+
+        //앞에 올수 있는 병사의 개수를 저장하는 배열
+        int[] count = new int[N];
+        int total = 0;
+        
+        //나보다 크면서 병사개수의 최대값을 갖고있는 배열 값에 + 1
+        for (int i = 0; i < N; i++) {
+            int max = 0;
+            for (int j = 0; j < i; j++) {
+                if (power[j] > power[i]) {
+                    max = Math.max(max, count[j]);
+                }
+            }
+
+            count[i] = max + 1;
+            total = Math.max(total, count[i]); //전체 배열에 대한 최대값을 갱신하면 선택한 병사수가 나옴
+        }
+
+        //답: 전체 병사수 - 선택 병사
+        System.out.println(N - total);
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        power = new int[N];
+        for (int i = 0; i < N; i++) {
+            power[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
+
