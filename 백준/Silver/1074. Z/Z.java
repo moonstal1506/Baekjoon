fa07f845b1d92da70e6c23bfaa9a4cca18d914ca
SuJeong Moon <moonstal1506@gmail.com>
2023-03-05 12:25:28 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,42 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static int[][] arr;
-	static int cnt = 0;
-	static int N;
-	static int R;
-	static int C;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int cnt = 0;
+    static int N;
+    static int R;
+    static int C;
 
-	public static void main(String[] args) throws IOException {
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		int size = (int) Math.pow(2, N);
-		cut(0, 0, size);
-	}
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        int size = (int) Math.pow(2, N);
+        cut(0, 0, size);
+    }
 
-	static void cut(int r, int c, int size) {
-		if (r == R && c == C) {
-			System.out.println(cnt);
-			return;
-		}
+    private static void cut(int r, int c, int size) {
+        //도착하면 출력
+        if (r == R && c == C) {
+            System.out.println(cnt);
+            return;
+        }
 
-		if (r <= R && R < (r + size) && c <= C && C < (c + size)) {
-			int half = size / 2;
-			cut(r, c, half);
-			cut(r, c + half, half);
-			cut(r + half, c, half);
-			cut(r + half, c + half, half);
-		}else {
-			cnt += size * size;
-		}
-	}
+        //포함되어있으면 또 잘라
+        if (r <= R && R < (r + size) && c <= C && C < (c + size)) {
+            int half = size / 2;
+            //순서 중요
+            cut(r, c, half);
+            cut(r, c + half, half);
+            cut(r + half, c, half);
+            cut(r + half, c + half, half);
+        } else {
+            //포함 아니면 사이즈*사이즈 증가
+            cnt += size * size;
+        }
+    }
+
 }
