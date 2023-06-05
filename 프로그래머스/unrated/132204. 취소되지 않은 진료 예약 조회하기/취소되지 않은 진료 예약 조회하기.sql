a6b5e7a4fb1ac0225594054c5be45af5dadc1b5a
SuJeong Moon <moonstal1506@gmail.com>
2023-06-05 15:49:48 +0900

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,10 @@
+SELECT APNT_NO, PT_NAME, a.PT_NO, a.MCDP_CD, DR_NAME, APNT_YMD
+from PATIENT p
+join APPOINTMENT a
+on p.PT_NO=a.PT_NO
+join DOCTOR d
+on a.MDDR_ID=d.DR_ID
+where APNT_YMD like "2022-04-13%"
+and a.MCDP_CD="CS"
+and APNT_CNCL_YN="N"
+order by APNT_YMD
