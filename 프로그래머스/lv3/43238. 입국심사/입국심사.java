a8fb17bd00e4156e59ebc6e063c44138dc29fc45
SuJeong Moon <moonstal1506@gmail.com>
2023-06-20 22:30:12 +0900

    [level 3] Title: 입국심사, Time: 0.43 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.java
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -5,16 +5,17 @@ class Solution {
         long answer = 0;
         Arrays.sort(times);
         long left = 0;
-        long right = (long) n * times[times.length - 1]; //가장 최악의 경우의(오래걸리는) 시간
+        long right = times[times.length - 1] * (long) n;
+
         while (left <= right) {
             long mid = (left + right) / 2;
-            long sum = 0; // 총 심사한 인원
-            for (int i = 0; i < times.length; i++) { //빨리 심사하는 심사관 순으로 심사처리
-                sum += mid / times[i];
+            long sum = 0;
+            for (int time : times) {
+                sum += mid / time;
             }
-            if (sum < n) { // 해야할 인원보다 심사처리 못함 -> 시간 더 필요
+            if (sum < n) {
                 left = mid + 1;
-            } else { // 해야할 인원보다 심사처리 많이함 -> 시간을 줄여서 더 최고 경우의 시간을 만든다.
+            } else {
                 right = mid - 1;
                 answer = mid;
             }
