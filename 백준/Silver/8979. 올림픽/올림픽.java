80e8a11c7486949ab65b6c5ff69e313ae8a80c99
SuJeong Moon <moonstal1506@gmail.com>
2024-03-10 18:38:25 +0900

    [Silver V] Title: 올림픽, Time: 148 ms, Memory: 14696 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,50 +13,67 @@ public class Main {
         String s = br.readLine();
         StringTokenizer st = new StringTokenizer(s, " ");
         int n = Integer.parseInt(st.nextToken());
-        int country = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[n + 1][4];
+        int nation = Integer.parseInt(st.nextToken());
+        List<Nation> nations = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
             String medal = br.readLine();
             st = new StringTokenizer(medal, " ");
-            int curCountry = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < 3; j++) {
-                arr[curCountry][j] = Integer.parseInt(st.nextToken());
-            }
+            int curNation = Integer.parseInt(st.nextToken());
+            int gold = Integer.parseInt(st.nextToken());
+            int silver = Integer.parseInt(st.nextToken());
+            int bronze = Integer.parseInt(st.nextToken());
+            nations.add(new Nation(curNation, gold, silver, bronze));
         }
 
-        findRank(n, arr, country);
+        Collections.sort(nations);
+        findRank(nations, nation);
     }
 
-    private static void findRank(int n, int[][] arr, int country) {
+    private static void findRank(List<Nation> nations, int nation) {
         int rank = 1;
-        int max = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int medal = 0; medal < 3; medal++) {
-            max = 0;
-            for (int j = 1; j <= n; j++) {
-                if (map.containsKey(j)) {
-                    continue;
-                }
-                max = Math.max(arr[j][medal], max);
-            }
-
-            int cnt = 0;
-            for (int j = 1; j <= n; j++) {
-                if (max == arr[country][medal]) {
-                    System.out.println(rank);
-                    return;
-                }
-
-                if (map.containsKey(j)) {
-                    continue;
-                }
-
-                if (max == arr[j][medal]) {
-                    map.put(j, rank);
-                    cnt++;
-                }
-            }
-            rank += cnt;
+        if (nation == nations.get(0).num) {
+            System.out.println(rank);
+            return;
         }
+
+        int cnt = 1;
+        for (int i = 1; i < nations.size(); i++) {
+            if (nations.get(i).gold == nations.get(i - 1).gold && nations.get(i).silver == nations.get(i - 1).silver && nations.get(i).bronze == nations.get(i - 1).bronze) {
+                cnt++;
+            } else {
+                rank += cnt;
+                cnt = 1;
+            }
+            if (nations.get(i).num == nation) {
+                System.out.println(rank);
+                return;
+            }
+        }
+    }
+
+}
+
+class Nation implements Comparable<Nation> {
+    int num, gold, silver, bronze;
+
+    public Nation(int num, int gold, int silver, int bronze) {
+        this.num = num;
+        this.gold = gold;
+        this.silver = silver;
+        this.bronze = bronze;
+    }
+
+    @Override
+    public int compareTo(Nation o) {
+        if (this.gold != o.gold) {
+            return o.gold - this.gold;
+        }
+
+        if (this.silver != o.silver) {
+            return o.silver - this.silver;
+        }
+
+        return o.bronze - this.bronze;
     }
 }
