8602fad8ac717f9a7ebc3ad6e95964da92e382bb
SuJeong Moon <moonstal1506@gmail.com>
2024-06-18 21:32:53 +0900

    [Silver IV] Title: 동전 0, Time: 152 ms, Memory: 16020 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int cnt = 0;
+        for (int i = n-1; i >= 0; i--) {
+            while (k >= arr[i]) {
+                k -= arr[i];
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
